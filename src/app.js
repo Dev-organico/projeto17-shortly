b4e2e5c2becf1359005946d378bca84836a88b62
Dev-organico <joao.zolet@gmail.com>
2023-02-27 17:48:12 -0300

    POST /signup and POST /signin

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import authRouter from "./routes/authRoute";
 
 
 const app = express();
@@ -7,7 +8,8 @@ app.use(cors());
 app.use(express.json());
 
 
-app.use([])
+app.use([authRouter])
 
+const port = 5000
 
-app.listen(port, () => console.log(`Server running on port ${process.env.PORT}`));
+app.listen(port, () => console.log(`Server running on port ${port}`));

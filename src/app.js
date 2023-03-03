5b1a96cfba5d567e1f96fb6ba92a6a9d21f9b352
Dev-organico <joao.zolet@gmail.com>
2023-03-03 16:02:00 -0300

    chore: adjust code for deploy on render

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 
-app.use([authRouter,shortlyRouter])
+app.use([authRouter,shortlyRouter]);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running in port: ${port}`));

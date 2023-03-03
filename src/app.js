05f0d77e9c28105f0c6e560c131b2249238bac98
Dev-organico <joao.zolet@gmail.com>
2023-03-03 15:32:49 -0300

    GET /ranking

--- src/app.js
+++ src/app.js
@@ -11,6 +11,5 @@ app.use(express.json());
 
 app.use([authRouter,shortlyRouter])
 
-const port = 5000
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running in port: ${port}`));
